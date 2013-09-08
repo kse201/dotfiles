c09aa65bdb19ff0d8672ee404cf8ebe3df1b49c7
kse201@windows7 <kse.201@gmail.com>
2013-09-08 16:41:13 +0900

    added 'unite-quifkfix plugin'

--- install.sh
+++ install.sh
@@ -1,25 +1,30 @@
 # tar cf dotfiles.tar $HOME/.bashrc $HOME/.zshrc $HOME/.vimrc $HOME/.gvimrc $HOME/.vimrc.plugin $HOME/.vim
 # rm -r  $HOME/.bashrc $HOME/.zshrc $HOME/.vimrc $HOME/.gvimrc $HOME/.vimrc.plugin $HOME/.vim
 
+BACKUP=$HOME/backup-`date %Y%m%d-%H%M%S`
+
+mkdir ${BACKUP}
+mv -r  $HOME/.bashrc $HOME/.zshrc $HOME/.vimrc $HOME/.gvimrc $HOME/.vimrc.plugin $HOME/.vim $HOME/.zsh.d $HOME/.zshenv ${BACKUP} 2>/dev/null
+
 ln -s $HOME/dotfiles/.bashrc $HOME/.bashrc
 ln -s $HOME/dotfiles/.zshrc  $HOME/.zshrc
 ln -s $HOME/dotfiles/.vimrc  $HOME/.vimrc
 ln -s $HOME/dotfiles/.gvimrc $HOME/.gvimrc
 ln -s $HOME/dotfiles/.vim    $HOME/.vim
+ln -s $HOME/dotfiles/.zsh.d  $HOME/.zsh.d
+ln -s $HOME/dotfiles/.zshenv $HOME/.zshenv
 
-if ![ -e $HOME/.vimbackup ] ; then
+if ! [ -e $HOME/.vimbackup ] ; then
     mkdir $HOME/.vimbackup
 fi
 
-mkdir -p $HOME/dotfiles/bundle
+mkdir -p $HOME/.vim/bundle
 git clone https://github.com/Shougo/neobundle.vim.git $HOME/.vim/bundle/neobundle.vim
-
 if [ $? != 0 ] ; then
     exit -1
 fi
 
 ln -s $HOME/dotfiles/.vimrc.plugin $HOME/.vimrc.plugin
-
 if [ $? != 0 ] ;then
     exit -1
 fi
