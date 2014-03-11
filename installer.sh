262a58f94e2c2d272f56a3c62819825b0214f297
se <se@RX300.(none)>
2014-03-11 14:54:57 +0900

    debuged installer

--- installer.sh
+++ installer.sh
@@ -1,15 +1,17 @@
-#!/bin/sh
+#!/bin/sh 
+
+2>/dev/null
 
 INSTALLER=$0
+DIR=`pwd`
 TARGET_FILES=`ls -1A | grep -v ${INSTALLER}`
 RETVAL=0
 
-
 dotfiles_backup() {
-    BACKUP=$HOME/backup_`date_+%Y%m%d-%H%M%S`.tar
+    BACKUP=$HOME/backup_`date +%Y%m%d_%H%M%S`.tar
     for file in ${TARGET_FILES} 
     do
-        tar --remove-files -rf ${BACKUP} $HOME/${file}
+        tar --remove-files -rf ${BACKUP} $HOME/${file} >/dev/null 2>&1
     done
 }
 
@@ -18,43 +20,50 @@ dotfiles_install() {
 
     for file in ${TARGET_FILES} 
     do
-        ln -s $HOME/.dotfiles/${file} $HOME/${file}
+        ln -s ${DIR}/${file} $HOME/${file} >/dev/null 2>&1
     done
+    echo "(1/3): dotfiles installed."
 
     mkdir -p $HOME/.vim/bundle
-    git clone https://github.com/Shougo/neobundle.vim.git $HOME/.vim/bundle/neobundle.vim
+    git clone https://github.com/Shougo/neobundle.vim.git $HOME/.vim/bundle/neobundle.vim >/dev/null 2>&1
     if [ $? != 0 ] ; then
-        return -1
+        echo "Error: failed git-clone neobundle.vim"
+        RETVAL=1
+        unlink $HOME/.vimrc.plugin
+	return
     fi
+    echo "(2/3): neobundle.vim installed."
 
-    git clone https://github.com/Shougo/unite.vim $HOME/.vim/bundle/unite.vim
+    git clone https://github.com/Shougo/unite.vim $HOME/.vim/bundle/unite.vim >/dev/null 2>&1
     if [ $? != 0 ] ; then
-        return -1
+        echo "Error: failed git-clone unite.vim"
+        RETVAL=1
+        unlink $HOME/.vimrc.plugin
+	return
     fi
+    echo "(3/3): unite.vim installed."
 
-    ln -s $HOME/.dotfiles/.vimrc.plugin $HOME/.vimrc.plugin
-    if [ $? != 0 ] ;then
-        return -1
-    fi
     vim -c "Unite neobundle/install"
 }
 
 dotfiles_uninstall(){
     for file in ${TARGET_FILES} 
     do
-        unlink $HOME/${file}
+        unlink $HOME/${file} >/dev/null 2>&1
     done
+    RETVAL=0
+    echo "Succecc: uninstall dotfiles."
 }
 
 case "$1" in
     install)
-        RETVAL = dotfiles_install
+        dotfiles_install
         ;;
     uninstall)
-        RETVAL = dotfiles_uninstall
+        dotfiles_uninstall
         ;;
     *)
         echo "Usage: $0 {install|uninstall}"
-        RETVAL = 1
+        RETVAL=1
 esac
 exit ${RETVAL}
