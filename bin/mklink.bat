d3577a9e7d877d9267d2507ea5e5f0dd692d1d88
kse <kse@Jonathan.local>
2014-03-18 06:16:37 +0900

    add install script in Windows.
        * add @himinato's mklink.bat.

---
+++ bin/mklink.bat
@@ -0,0 +1,5 @@
+mklink %HOMEPATH%"\.vimrc" %HOMEPATH%"\dotfiles\.vimrc"
+mklink /D %HOMEPATH%"\.vim\ftdetect" %HOMEPATH%"\dotfiles\ftdetect"
+mklink /D %HOMEPATH%"\.vim\indent" %HOMEPATH%"\dotfiles\indent"
+mklink /D %HOMEPATH%"\.vim\colors" %HOMEPATH%"\dotfiles\colors"
+exit 0
