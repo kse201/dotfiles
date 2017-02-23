afabf2bd9f6102dc59db729ea974dba069c2b811
kse201 <kse.201@gmail.com>
2017-02-23 23:01:34 +0900

    feature: add fish config

---
+++ fish/config.fish
@@ -0,0 +1,7 @@
+set PATH $HOME/bin /usr/local/bin /usr/local/sbin $PATH
+set LANG en_US.UTF-8
+set LESSCHARSET utf-8
+set EDITOR 'vim'
+set HISTSIZE 7500
+set SAVEHIST 7500
+set HISTIGNORE "ls *:cd:history:fg*:history-all"
