cc6c19439ace9123d6a2188464729af1307c4c46
kse201 <kse.201@gmail.com>
2017-02-27 16:35:21 +0900

    feature: add rbenv settings for fish

--- fish/config.fish
+++ fish/config.fish
@@ -1,7 +1,10 @@
-set -x PATH $HOME/bin /usr/local/bin /usr/local/sbin $PATH
+set -x PATH $HOME/bin /usr/local/bin /usr/local/sbin /usr/local/rbenv/bin $PATH
 set -x LANG ja_JP.UTF-8
 set -x LESSCHARSET utf-8
 set -x EDITOR 'vim'
 set -x HISTSIZE 7500
 set -x SAVEHIST 7500
 set -x HISTIGNORE "ls *:cd:history:fg*:history-all"
+
+set -x RBENV_ROOT /usr/local/rbenv
+rbenv init - | source
