52c9a7a9baf1672cfac972d802a532afdbea35a9
kse201 <kse.201@gmail.com>
2017-03-19 20:37:45 +0900

    bugfix: use rbenv user local

--- fish/config.fish
+++ fish/config.fish
@@ -1,13 +1,10 @@
-set -x PATH $HOME/bin /usr/local/bin /usr/local/sbin /usr/local/rbenv/bin $PATH
+set -x PATH $HOME/bin /usr/local/bin /usr/local/sbin $PATH
 set -x LANG ja_JP.UTF-8
 set -x LESSCHARSET utf-8
 set -x EDITOR 'vim'
 set -x HISTSIZE 7500
 set -x SAVEHIST 7500
 set -x HISTIGNORE "ls *:cd:history:fg*:history-all"
-
-set -x RBENV_ROOT /usr/local/rbenv
-rbenv init - | source
 
 abbr vi vim
 
@@ -16,3 +13,14 @@ abbr vup vagrant up
 
 abbr gst git status
 abbr glg git lg
+
+if test -e $HOME/.rbenv
+  set -x RBENV_ROOT $HOME/.rbenv
+else
+  set -x RBENV_ROOT /usr/local/rbenv/bin
+end
+
+rbenv init - | source
+if test -e $HOME/.ruby-version
+  set PATH $HOME/.gem/ruby/(cat $HOME/.ruby-version)/bin $PATH
+end
